2cf891895ff62964819675748a64f45fcca890d7
antonio gamba-bari <lodeanto@gmail.com>
2025-01-21 15:43:54 -0500

    Removed custon tables for error logging

--- models.py
+++ models.py
@@ -30,7 +30,6 @@ class Job(Base):
     __tablename__ = 'jobs'
     id = Column(Integer, primary_key=True, autoincrement=False)
     job = Column(String(255))
-    transaction_id = Column(String(255))
 
 class HiredEmployee(Base):
     __tablename__ = 'hired_employees'
@@ -42,10 +41,6 @@ class HiredEmployee(Base):
     department_id = Column(Integer)
     job_id = Column(Integer)
 
-# logging tables used for storing invalid data 
-# having a separate table for each base table seems more efficient for later analisys
-# TODO: consier if we need to log the original csv line of data that failed 
-#####################
 
 class BackupFile(Base):
     __tablename__ = 'backups_files'
@@ -54,36 +49,11 @@ class BackupFile(Base):
     datetime = Column(DateTime)
     avro_file = Column(String(255))
 
-class DepartmentFailed(Base):
-    __tablename__ = 'departments_failed'
-    row_id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(String(255))
-    department = Column(String(255))
-    errors = Column(String(255))
-
-class JobFailed(Base):
-    __tablename__ = 'jobs_failed'
-    row_id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(String(255))
-    job = Column(String(255))
-    errors = Column(String(255))
-
-class HiredEmployeeFailed(Base):
-    __tablename__ = 'hired_employees_failed'
-    # id = Column(Integer, primary_key=True, autoincrement=False)
-    row_id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(String(255))
-    name = Column(String(255))
-    datetime = Column(String(255))
-    department_id = Column(String(255))
-    job_id = Column(String(255))
-    errors = Column(String(255))
+# force delete all tables for development
+Base.metadata.drop_all(engine)
 
 # Create tables if do not exist
 Base.metadata.create_all(engine)
-
-def drop_all_tables():
-    Base.metadata.drop_all(engine)
 
 # bind session to engine
 Session = sessionmaker(bind=engine)
