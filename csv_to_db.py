067597ee909c5ada142677dea53e0ac18724ae70
antonio gamba-bari <lodeanto@gmail.com>
2025-01-21 12:29:48 -0500

    Added validation rules for hired_employees

--- csv_to_db.py
+++ csv_to_db.py
@@ -19,7 +19,7 @@ def load_csv_data(file_name, chunk_size, table_name):
     columns_names = columns_names_by_table[table_name]
     # optional, Specify columns data types: e.g. dtype={'id': 'int64', }
     df_chunks = pd.read_csv(file_name, chunksize=chunk_size, names=columns_names)
-    print("Padas chunks csv", df_chunks)
+    print("Padas parsers object: ", df_chunks)
     return df_chunks
 
 def process_df_chunks(df_chunks, table_name):
@@ -30,6 +30,36 @@ def process_df_chunks(df_chunks, table_name):
         # consider separating valid and invalid data in df and process them separately
         # validate_data(df, table_name)
 
+        # before start make a full copy of the df. to be used for logging purposes
+        df_copy = df.copy()
+        
+        """Define validation rules for each table"""
+        try:
+            print()
+            if(table_name=="hired_employees"):
+                valid_data, invalid_data = validate_hired_employees(df)
+                # print("valid_data", valid_data)
+                # print("invalid_data", invalid_data)
+                print("LEN valid_data", len(valid_data))
+                print("LEN invalid_data", len(invalid_data))
+                
+            elif(table_name=="departments"):
+                valid_data, invalid_data = validate_departments(df)
+                
+            elif(table_name=="jobs"):
+                valid_data, invalid_data = validate_jobs(df)
+                
+            else:
+                print("table name not found")
+
+
+        except Exception as e:
+            error_message = f"\nValidating batch. error: {e}"
+            error_message += f"\nTraceback:\n{traceback.format_exc()}"
+            print(error_message)
+            return None
+        
+
         # 2. TODO: insert valid data in sql 
         # insert_data(df, table_name)
         # consider using pandas to_sql() or other SQLAlchemy methods
@@ -37,11 +67,35 @@ def process_df_chunks(df_chunks, table_name):
 
         # 3. TODO: handle errors
 
-        print("---")
-        print(df)
     
     return results
 
+def validate_hired_employees(df):
+    print("\tvalidate_hired_employees()")
+    # copy datetime column to new column for logging purposes and validating duplicates
+    df['datetime_str'] = df['datetime'].copy()
+
+    """ convert columns to appropriate data types so all invalid data is NaN"""
+    df['id'] = pd.to_numeric(df['id'], errors='coerce').astype('Int64')  # Nullable integer type
+    df['department_id'] = pd.to_numeric(df['department_id'], errors='coerce').astype('Int64')
+    df['job_id'] = pd.to_numeric(df['job_id'], errors='coerce').astype('Int64')
+    df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')  # convert datetime to pd format
+
+    # Create a new df with all rows with NaN values
+    invalid_data = df[df.isnull().any(axis=1)]
+
+    # Remove rows with NaNs in all required fields so we have only the valid data
+    df.dropna(subset=['id', 'name', 'datetime', 'department_id', 'job_id'], inplace=True)
+    
+    results = (df, invalid_data)
+    return results
+
+def validate_departments(df):
+    return 
+
+
+def validate_jobs(df):
+    return
 
 #################################
 # load db session
@@ -68,7 +122,7 @@ file_name = './data/hired_employees.csv'
 # table_name = "jobs"
 # file_name = './data/jobs.csv'
 
-chunk_size = 4
+chunk_size = 1000
 
 ################################
 # 1. get data batches
