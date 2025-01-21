a9df5bfe80eb14886826de0ed3ac2af411b0e5ab
antonio gamba-bari <lodeanto@gmail.com>
2025-01-21 15:45:46 -0500

    Run tests with different types of invalid data

--- util.py
+++ util.py
@@ -6,15 +6,15 @@ table_name = "hired_employees"
 file_name = './data/hired_employees.csv'
 file_name = './data/hired_employees_error.csv'
 
-# table_name = "departments"
-# file_name = './data/departments.csv'
-# file_name = './data/departments_error.csv'
+table_name = "departments"
+file_name = './data/departments.csv'
+file_name = './data/departments_error.csv'
 
 # table_name = "jobs"
 # file_name = './data/jobs.csv'
 # file_name = './data/jobs_error.csv'
 
-chunk_size = 1000
+chunk_size = 200
 
 # Define the column names for each table
 # assuming that csv does not have header
