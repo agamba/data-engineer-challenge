042e92ab4fd9e83cfe18a9f427d598494f90a002
antonio gamba-bari <lodeanto@gmail.com>
2025-01-21 20:25:10 -0500

    Testing simple validation for API params

--- main.py
+++ main.py
@@ -10,28 +10,39 @@ from csv_to_db import *
 
 app = Flask(__name__, template_folder='templates')
 
+def validate_request(data):
+    """
+    Validate the request data to ensure all mandatory fields are present.
+    :param data: The request data as a dictionary.
+    :return: True if all mandatory fields are present, False otherwise.
+    """
+    # mandatory fields for data import requests
+    mandatory_fields = ['file_name', 'chunk_size', 'table_name']
+    for field in mandatory_fields:
+        if field not in data and field != "":
+            return False
+    return True
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
 @app.route("/departments", methods=['POST'])
 def get_departments():
-
     data = request.get_json()
-    file_name_q = data.get('file_name')
-    chunk_size = data.get('chunk_size')
-    table_name = data.get('table_name')
-    # print(file_name_q, chunk_size, table_name)
-
-    result_logs, file_path = process_valid_invalid_results(file_name_q, chunk_size, table_name)
-    response = {
-        "parameters": data,
-        "message": "Departments endpoint",
-        "result_logs": result_logs,
-        "logs_file_path": file_path
-    }
-    return jsonify(response), 201
+    if(validate_request(data)):
+        file_name, chunk_size, table_name = data
+        print(file_name, chunk_size, table_name)
+        result_logs, file_path = process_valid_invalid_results(file_name, chunk_size, table_name)
+        response = {
+            "parameters": data,
+            "message": "Departments endpoint",
+            "result_logs": result_logs,
+            "logs_file_path": file_path
+        }
+        return jsonify(response), 201
+    else:
+        return jsonify({"error": f"Missing mandatory fields"}), 400
 
 
 @app.route("/jobs", methods=['POST'])
@@ -90,8 +101,7 @@ def create_backups():
     return jsonify(response)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 
