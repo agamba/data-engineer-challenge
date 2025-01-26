b29a0e9ee2ea8ade105ff12f70b8e40b72c285d6
antonio gamba-bari <lodeanto@gmail.com>
2025-01-25 19:55:58 -0500

    Updated Plan for automated tests/stress API

--- tests/01_import_sample.sh
+++ tests/01_import_sample.sh
@@ -1,3 +1,4 @@
+# IMPORT GIVEN DATA
 curl -X POST -F "file=@data/departments.csv" \
 -F "table_name=departments" \
 -F "chunk_size=1000" http://127.0.0.1:8080/import
@@ -9,3 +10,14 @@ curl -X POST -F "file=@data/jobs.csv" \
 curl -X POST -F "file=@data/hired_employees.csv" \
 -F "table_name=hired_employees" \
 -F "chunk_size=1000" http://127.0.0.1:8080/import
+
+
+
+# TODO: AUTOMATION WITH MULTILE TYPES OF DATA
+
+
+# IMPORT ERROR DATA
+
+# IMPORT MIXED VALID/ERROR DATA
+
+# IMPORT AI GEN DATA
