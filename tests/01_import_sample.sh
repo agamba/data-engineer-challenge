ad997782e4dd1b22fd5de891fa26ff57a3019e5a
antonio gamba-bari <lodeanto@gmail.com>
2025-01-27 15:04:18 -0500

    updated test script for automated import of sample data

--- tests/01_import_sample.sh
+++ tests/01_import_sample.sh
@@ -12,12 +12,12 @@ curl -X POST -F "file=@data/hired_employees.csv" \
 -F "chunk_size=1000" http://127.0.0.1:8080/import
 
 # RESTORE BACKUCP
-curl -X POST \
--F "restore_file_name=departments___d51850d6-0974-4563-80d9-4f437fecd8b6.avro" \
-http://127.0.0.1:8080/backups-restore
+# curl -X POST \
+# -F "restore_file_name=departments___d51850d6-0974-4563-80d9-4f437fecd8b6.avro" \
+# http://127.0.0.1:8080/backups-restore
 
 
 # CREATE BACKUCP
-curl -X POST \
--F "table_name=departments" \
-http://127.0.0.1:8080/backups-create
+# curl -X POST \
+# -F "table_name=departments" \
+# http://127.0.0.1:8080/backups-create
