87ab46e4f1f67d39a9942059016214c9f8c16ccf
antonio gamba-bari <lodeanto@gmail.com>
2025-01-27 14:24:55 -0500

    Added results sample images

--- tests/01_import_sample.sh
+++ tests/01_import_sample.sh
@@ -1,4 +1,4 @@
-# IMPORT GIVEN DATA
+# IMPORT DATA
 curl -X POST -F "file=@data/departments.csv" \
 -F "table_name=departments" \
 -F "chunk_size=1000" http://127.0.0.1:8080/import
@@ -11,13 +11,13 @@ curl -X POST -F "file=@data/hired_employees.csv" \
 -F "table_name=hired_employees" \
 -F "chunk_size=1000" http://127.0.0.1:8080/import
 
+# RESTORE BACKUCP
+curl -X POST \
+-F "restore_file_name=departments___d51850d6-0974-4563-80d9-4f437fecd8b6.avro" \
+http://127.0.0.1:8080/backups-restore
 
 
-# TODO: AUTOMATION WITH MULTILE TYPES OF DATA
-
-
-# IMPORT ERROR DATA
-
-# IMPORT MIXED VALID/ERROR DATA
-
-# IMPORT AI GEN DATA
+# CREATE BACKUCP
+curl -X POST \
+-F "table_name=departments" \
+http://127.0.0.1:8080/backups-create
